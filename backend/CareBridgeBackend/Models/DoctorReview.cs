c13258c95744a543ee84849481022466b032f117
VenicioAugusto <79486034+VenicioAugusto@users.noreply.github.com>
2025-01-29 21:13:34 -0500

    Add DoctorReview entity

---
+++ backend/CareBridgeBackend/Models/DoctorReview.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace CareBridgeBackend.Models
+{
+    public class DoctorReview
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int DoctorId { get; set; }
+
+        [ForeignKey("DoctorId")]
+        public User Doctor { get; set; }
+
+        [Required]
+        public int PatientId { get; set; }
+
+        [ForeignKey("PatientId")]
+        public User Patient { get; set; }
+
+        [Required]
+        [Range(1, 5)]
+        public int Rating { get; set; }
+
+        [Required]
+        [StringLength(500)]
+        public string ReviewText { get; set; }
+
+        public DateTime ReviewDate { get; set; } = DateTime.UtcNow;
+    }
+}
