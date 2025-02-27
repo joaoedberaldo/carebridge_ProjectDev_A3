4a4e2ee53f2cd0bed23ceee44823fbcaf10942f2
VenicioAugusto <79486034+VenicioAugusto@users.noreply.github.com>
2025-02-26 19:15:30 -0500

    Add Medication model

---
+++ backend/CareBridgeBackend/Models/Medication.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace CareBridgeBackend.Models
+{
+    public class Medication
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int PrescriptionId { get; set; }
+        [ForeignKey(nameof(PrescriptionId))]
+        public Prescription Prescription { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Dosage { get; set; }  
+
+        [Required]
+        [StringLength(50)]
+        public string Frequency { get; set; }  
+
+        [StringLength(500)]
+        public string? Notes { get; set; }  
+    }
+}
