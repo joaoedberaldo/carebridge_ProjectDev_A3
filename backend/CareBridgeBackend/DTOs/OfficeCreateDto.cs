5f97dda475ca544903ac4cfcb3a4a3f1a2b8c073
VenicioAugusto <79486034+VenicioAugusto@users.noreply.github.com>
2025-03-06 22:07:15 -0500

    Add Office related Dtos

---
+++ backend/CareBridgeBackend/DTOs/OfficeCreateDto.cs
@@ -0,0 +1,10 @@
+﻿namespace CareBridgeBackend.DTOs
+{
+    public class OfficeCreateDto
+    {
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+    }
+}
