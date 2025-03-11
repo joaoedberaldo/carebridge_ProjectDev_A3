af76f3715d126e6f0e08533bc355a0f3ad2da1fd
VenicioAugusto <79486034+VenicioAugusto@users.noreply.github.com>
2025-03-11 17:24:47 -0400

    Add DoctorReviewControllerTests and fixed the related controller

---
+++ backend/CareBridgeBackend/DTOs/DoctorReviewCreateDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CareBridgeBackend.DTOs
+{
+    public class DoctorReviewCreateDto
+    {
+        [Required]
+        [Range(1, 5)]
+        public int Rating { get; set; }
+
+        [Required]
+        [StringLength(500)]
+        public string ReviewText { get; set; }
+    }
+}
